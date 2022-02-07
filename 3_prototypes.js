c2914ee009883e303b77ab506e10948e7750c169
Austin Clements <austinclementsbass@gmail.com>
2022-02-07 08:55:47 -0600

    complete lesson 3

--- 3_prototypes.js
+++ 3_prototypes.js
@@ -14,10 +14,16 @@ Book.prototype.getAge = function() {
     return `${this.title} is ${years} years old`;
 };
 
+Book.prototype.revise = function(newYear) {
+    this.year = newYear
+    this.revised = true;
+}
+
 // Instantiate Objects
 const book1 = new Book('Book One', 'John Doe', '2013');
 const book2 = new Book('Book Two', 'Jane Doe', '2016');
 
-console.log(book1.getSummary());
-console.log(book1.getAge());
+console.log(book2);
+book2.revise('2018');
+console.log(book2);
 
