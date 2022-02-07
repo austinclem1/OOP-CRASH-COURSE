ccf21f6778f85a45743023f04e4633abe1e91aa8
Austin Clements <austinclementsbass@gmail.com>
2022-02-07 09:05:19 -0600

    do lesson 4 inheritance

--- 4_inheritance.js
+++ 4_inheritance.js
@@ -1,4 +1,4 @@
-// Constructor
+// Book Constructor
 function Book(title, author, year) {
     this.title = title;
     this.author = author;
@@ -9,3 +9,18 @@ Book.prototype.getSummary = function() {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 };
 
+// Magazine Constructor
+function Magazine(title, author, year, month) {
+    Book.call(this, title, author, year);
+    this.month = month;
+}
+
+// Inherit Prototype
+Magazine.prototype = Object.create(Book.prototype);
+Magazine.prototype.constructor = Magazine;
+
+// Instantiate Magazine Object
+const mag1 = new Magazine('Mag One', 'John Doe', '2018', 'Jan');
+
+console.log(mag1);
+console.log(mag1.getSummary());
