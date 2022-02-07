129389baf85fc10651acdb18cdd3be4d55f52c06
Austin Clements <austinclementsbass@gmail.com>
2022-02-07 09:13:47 -0600

    complete object create lesson 5

---
+++ 5_object_create.js
@@ -0,0 +1,18 @@
+// Object of Protos
+const bookProtos = {
+    getSummary: function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    },
+    getAge: function() {
+        const years = new Date().getFullYear() - this.year;
+        return `${this.title} is ${years} years old`;
+    }
+};
+
+const book1 = Object.create(bookProtos, {
+    title: { value: 'Book One' },
+    author: { value: 'John Doe' },
+    year: { value: '2018' }
+});
+
+console.log(book1);
